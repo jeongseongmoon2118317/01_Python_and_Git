debe65f37ca8b4ec0c884e4ffc189d21a440c919
jeongseongmoon <b4jsm2@naver.com>
2024-09-13 17:40:35 +0900

    파이썬
    헬로우 월드 삽입

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+print("hello world")
+name = input("이름을 입력하세요.")
+input(name)
+git config "b4jsm2@naver.com"
+git config "jeongseongmoon"
+
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
